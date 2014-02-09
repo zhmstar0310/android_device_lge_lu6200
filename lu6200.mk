5970eb5a375f6c667f4e0947272e85a3685007ec
zhmstar0310 <zhmstar0310@gmail.com>
2014-02-09 19:30:44 +0000

    add NFC

--- lu6200.mk
+++ lu6200.mk
@@ -11,6 +11,18 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.iprj.rc:root/init.iprj.rc
 
+## NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/nfcee_access.xml:system/etc/nfcee_access.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag
+
 PRODUCT_NAME := full_lu6200 
 PRODUCT_DEVICE := lu6200 
 PRODUCT_MODEL := LG-lu6200 
